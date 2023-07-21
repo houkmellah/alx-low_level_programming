762587afb4211a2ddd9ca5edf99d52a311326bf6
houkmellah <houkmellah@gmail.com>
2023-07-20 17:03:26 -0700

    fix error 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - causes a loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 int main(void)
 {
-    int i;
-
-    printf("Loop incoming :(\n");
-    i = 0;
-    while (i < 10)
-    {
-        putchar('0' + i);
-        i++;
-    }
-    printf("\nLoop avoided! \\o/\n");
-    return (0);
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
