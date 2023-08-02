a6a3f10c6306f71bc828d93eabca3156ef710057
houkmellah <houkmellah@gmail.com>
2023-08-02 16:32:22 -0700

    final commit

---
+++ 3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - Returns the factorial of a number.
+ * @n: The number to calculate the factorial of.
+ *
+ * Return: The factorial of n. If n is negative, returns -1.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
---
+++ 5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to calculate the square root of.
+ *
+ * Return: The resulting square root. If n is negative, returns -1.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - Recurses to find the natural square root of a number.
+ * @n: The number to calculate the square root of.
+ * @i: Iterator.
+ *
+ * Return: The resulting square root. If no exact square root is found, returns -1.
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
