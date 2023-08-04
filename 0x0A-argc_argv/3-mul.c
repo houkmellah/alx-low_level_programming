0fa37294796ed3337b9531a62bb24b3df1fc2bbc
houkmellah <houkmellah@gmail.com>
2023-08-04 14:27:47 -0700

    first commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -54,19 +54,19 @@ return (n);
  */
 int main(int argc, char *argv[])
 {
-    int result, num1, num2;
+int result, num1, num2;
 
-    if (argc < 3 || argc > 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
+if (argc < 3 || argc > 3)
+{
+printf("Error\n");
+return (1);
+}
 
-    num1 = _atoi(argv[1]);
-    num2 = _atoi(argv[2]);
-    result = num1 * num2;
+num1 = _atoi(argv[1]);
+num2 = _atoi(argv[2]);
+result = num1 *num2;
 
-    printf("%d\n", result);
+printf("%d\n", result);
 
-    return (0);
+return (0);
 }
