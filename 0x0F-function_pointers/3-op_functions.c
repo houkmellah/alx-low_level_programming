f541c217ee99fdc13aa130e0967de0716012d700
houkmellah <houkmellah@gmail.com>
2023-08-16 18:45:12 -0700

    second commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,56 +7,60 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - Returns the sum of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The sum of a and b.
- */
+* op_add - Computes sum of two integers.
+* @a: First integer.
+* @b: Second integer.
+*
+* Return: Sum of a and b.
+*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - Returns the difference of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The difference of a and b.
- */
+* op_sub - Computes difference between two integers.
+* @a: First integer.
+* @b: Second integer.
+*
+* Return: Difference between a and b.
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - Returns the product of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The product of a and b.
- */
+* op_mul - Computes product of two integers.
+* @a: First integer.
+* @b: Second integer.
+*
+* Return: Product of a and b.
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - Returns the division of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The quotient of a and b.
- */
+* op_div - Computes division result of two integers.
+* @a: Numerator.
+* @b: Denominator.
+*
+* Return: Quotient of a divided by b.
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
 /**
- * op_mod - Returns the remainder of the division of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The remainder of the division of a by b.
- */
+* op_mod - Computes modulus of two integers.
+* @a: Dividend.
+* @b: Divisor.
+*
+* Return: Remainder when a is divided by b.
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);
