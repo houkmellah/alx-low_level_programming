b8de29c105f06c87621c9571d9e0fe017bbbc508
houkmellah taha <houkmellah@gmail.com>
2023-08-31 16:59:12 -0700

    fix error

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,17 +3,17 @@
 * print_binary - Outputs binary of a decimal number.
 * @n: Decimal number.
 */
-void print_binary(unsigned long int num){
+void print_binary(unsigned long int n){
 int i,flag=0;
 unsigned long int shift;
 for(i=63;i>=0;i--){
-shift=num>>i;
+shift=n>>i;
 if(shift&1){
 _putchar('1');
 flag++;
-}else if(flag)
+}else if(flag){
 _putchar('0');
 }
-if(!flag)
-_putchar('0');
+}
+if(!flag)_putchar('0');
 }
