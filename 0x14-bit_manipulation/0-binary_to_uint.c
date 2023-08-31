ce72434906ead50d935b11d379c0251e0eb2d846
houkmellah taha <houkmellah@gmail.com>
2023-08-31 16:52:44 -0700

    fix error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,15 +4,17 @@
 * @b: Binary string.
 * Return: Transformed unsigned int.
 */
-unsigned int binary_to_uint(const char *binStr) {
+unsigned int binary_to_uint(const char *b)
+{
 int idx;
 unsigned int uintVal = 0;
-if (!binStr)
-return 0;
-for (idx = 0; binStr[idx]; idx++) {
-if (binStr[idx] < '0' || binStr[idx] > '1')
-return 0;
-uintVal = 2 * uintVal + (binStr[idx] - '0');
+if (!b)
+return (0);
+for (idx = 0; b[idx]; idx++)
+{
+if (b[idx] < '0' || b[idx] > '1')
+return (0);
+uintVal = 2 * uintVal + (b[idx] - '0');
 }
-return uintVal;
+return (uintVal);
 }
