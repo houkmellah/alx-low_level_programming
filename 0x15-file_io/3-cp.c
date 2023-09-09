2c5cc4eec9c89170be497c40c98b00e7059190e5
houkmellah taha <houkmellah@gmail.com>
2023-09-08 17:38:39 -0700

    fix error 2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,18 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 char *allocateBuffer(char *filename);
 void closeFileDescriptor(int fd);
 
 /**
-* allocateBuffer - Allocates 1024 bytes for a temp buffer.
-* @filename: The name of the file the buffer is used for.
-*
-* Return: Pointer to the newly-allocated buffer.
-*/
+ * allocateBuffer - Allocates 1024 bytes for a temp buffer.
+ * @filename: The name of the file the buffer is used for.
+ *
+ * Return: Pointer to the newly-allocated buffer.
+ */
 char *allocateBuffer(char *filename)
 {
 char *tempBuffer;
@@ -26,9 +24,9 @@ return (tempBuffer);
 }
 
 /**
-* closeFileDescriptor - Closes given file descriptor.
-* @fd: The file descriptor to close.
-*/
+ * closeFileDescriptor - Closes given file descriptor.
+ * @fd: The file descriptor to close.
+ */
 void closeFileDescriptor(int fd)
 {
 int closeStatus;
@@ -41,59 +39,47 @@ exit(100);
 }
 
 /**
-* main - Duplicates file content to another file.
-* @argc: Number of arguments provided.
-* @argv: Array of pointers to the arguments.
-*
-* Return: 0 on success.
-* Description: If incorrect arg count - exit 97.
-* If source file issues - exit 98.
-* If destination file issues - exit 99.
-* If file close issues - exit 100.
-*/
+ * main - Duplicates file content to another file.
+ * @argc: Number of arguments provided.
+ * @argv: Array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ * Description: If incorrect arg count - exit 97.
+ * If source file issues - exit 98.
+ * If destination file issues - exit 99.
+ * If file close issues - exit 100.
+ */
 int main(int argc, char *argv[])
 {
 int srcFD, destFD, readCount, writeCount;
 char *buffer;
-
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp source dest\n");
 exit(97);
 }
-
 buffer = allocateBuffer(argv[2]);
 srcFD = open(argv[1], O_RDONLY);
-
-if (srcFD == -1)
+readCount = read(srcFD, buffer, 1024);
+destFD = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+do {
+if (srcFD == -1 || readCount == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
-
-destFD = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, 0664);
-
-if (destFD == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buffer);
-exit(99);
-}
-
-while ((readCount = read(srcFD, buffer, 1024)) > 0)
-{
 writeCount = write(destFD, buffer, readCount);
-if (writeCount != readCount)
+if (destFD == -1 || writeCount == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 free(buffer);
-closeFileDescriptor(srcFD);
-closeFileDescriptor(destFD);
 exit(99);
 }
-}
-
+readCount = read(srcFD, buffer, 1024);
+destFD = open(argv[2], O_WRONLY | O_APPEND);
+} while (readCount > 0);
 free(buffer);
 closeFileDescriptor(srcFD);
 closeFileDescriptor(destFD);
