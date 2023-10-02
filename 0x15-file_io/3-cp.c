bf2900777bbb8d9c44d2ea2ac617a1625cd58263
root <houkmellah@gmail.com>
2023-10-02 13:57:00 -0700

    change task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,94 +2,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file_name);
-void close_file_descriptor(int file_desc);
+char *allocate_buffer(char *filename);
+void shut_file(int file_desc);
 
 /**
- * create_buffer - Allocates memory for a buffer of 1024 bytes.
- * @file_name: File name for which the buffer stores characters.
+ * allocate_buffer - Assigns memory for a buffer of size 1024 bytes.
+ * @filename: Name of the file the buffer is associated with.
  *
- * Return: A pointer to the allocated buffer.
+ * Return: Pointer to the allocated buffer.
  */
-char *create_buffer(char *file_name)
+char *allocate_buffer(char *filename)
 {
 char *buf;
 buf = malloc(sizeof(char) * 1024);
-if (buf == NULL)
+if (!buf)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
+dprintf(STDERR_FILENO, "Error: Cannot write to %s\n", filename);
 exit(99);
 }
 return (buf);
 }
 
 /**
- * close_file_descriptor - Closes a file descriptor.
+ * shut_file - Safely closes the file descriptor.
  * @file_desc: File descriptor to close.
  */
-void close_file_descriptor(int file_desc)
+void shut_file(int file_desc)
 {
-int status;
-status = close(file_desc);
-if (status == -1)
+if (close(file_desc) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_desc);
+dprintf(STDERR_FILENO, "Error: Cannot close fd %d\n", file_desc);
 exit(100);
 }
 }
 
 /**
- * main - Copies content from one file to another.
- * @argc: Count of arguments provided to the program.
- * @argv: Array of pointers to the provided arguments.
+ * main - Duplicates content from one file to another.
+ * @argc: Argument count.
+ * @argv: Argument values.
  *
- * Return: 0 on success.
- * Description: Exit codes:
- * 97 if incorrect argument count.
- * 98 if source file doesn't exist or isn't readable.
- * 99 if destination file isn't creatable or writable.
- * 100 if a file can't be closed.
+ * Return: 0 if successful.
+ *
+ * Errors: 97 for incorrect argument count.
+ *         98 if source file doesn't exist or can't be read.
+ *         99 if target file can't be created or written to.
+ *         100 if either file can't be closed.
  */
 int main(int argc, char *argv[])
 {
-int source_fd, dest_fd, read_bytes, written_bytes;
+int src, dest, bytesRead, bytesWritten;
 char *buf;
 
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp source dest\n");
+dprintf(STDERR_FILENO, "Usage: cp source destination\n");
 exit(97);
 }
 
-buf = create_buffer(argv[2]);
-source_fd = open(argv[1], O_RDONLY);
-read_bytes = read(source_fd, buf, 1024);
-dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+buf = allocate_buffer(argv[2]);
+src = open(argv[1], O_RDONLY);
+bytesRead = read(src, buf, 1024);
+dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-do {
-if (source_fd == -1 || read_bytes == -1)
+do 
+{
+if (src == -1 || bytesRead == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 free(buf);
 exit(98);
 }
 
-written_bytes = write(dest_fd, buf, read_bytes);
-if (dest_fd == -1 || written_bytes == -1)
+bytesWritten = write(dest, buf, bytesRead);
+if (dest == -1 || bytesWritten == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 free(buf);
 exit(99);
 }
 
-read_bytes = read(source_fd, buf, 1024);
-dest_fd = open(argv[2], O_WRONLY | O_APPEND);
+bytesRead = read(src, buf, 1024);
+dest = open(argv[2], O_WRONLY | O_APPEND);
 
-} while (read_bytes > 0);
+} while (bytesRead > 0);
 
 free(buf);
-close_file_descriptor(source_fd);
-close_file_descriptor(dest_fd);
+shut_file(src);
+shut_file(dest);
 
 return (0);
 }
